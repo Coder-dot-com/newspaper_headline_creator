7abb236c224b4195501817632b8575d79b89337c
Usman <usman17.s@hotmail.com>
2024-03-31 20:57:40 +0100

    Update databses

--- src/newspaper_headline_creator/settings.py
+++ src/newspaper_headline_creator/settings.py
@@ -197,15 +197,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'newspaper_headline_creator.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
